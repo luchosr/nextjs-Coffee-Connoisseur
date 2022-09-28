d21cc757bcaed7826c0e5ccb6daeafeb93509b8f
Luciano Ramello <luchosr@gmail.com>
2022-09-28 11:12:29 +0200

    update hero image and styles

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Banner from '../components/banner';
 import styles from '../styles/Home.module.css';
 
@@ -17,6 +18,14 @@ export default function Home() {
           buttonText='View stores nearby'
           handleOnClick={handleOnBannerBtnClick}
         />
+        <div className={styles.heroImage}>
+          <Image
+            src='/static/hero-image.png'
+            alt='lady with coffee mug'
+            width={700}
+            height={400}
+          />
+        </div>
       </main>
     </div>
   );
